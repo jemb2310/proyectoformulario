db58aa940ec78dd9002a47cd15fe3e7dd7e9b739
jemb2310 <juanelbertomartin@gmail.com>
2016-11-17 16:47:30 +0100

    creamos un proyecto con un formulario

--- index.php
+++ index.php
@@ -9,8 +9,21 @@ Ejemplo de un formulario un poquito mas completo
         <title>Ejemplo de formulario de alta</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <form action="datos.php" method="POST">
+            Nombre: <input type="text" name="nombre"><br>
+            Edad: <input type="number" name="edad"><br>
+            Ciudad: <select name="ciudad"><br>
+                <option>Barcelona</option>
+                <option>Girona</option>
+                <option>Lleida</option>
+                <option>Tarragona</option>
+            </select><br>
+            
+            Sexo: 
+            Hombre <input type="radio" name="sexo" value="hombre">
+            Mujer <input type="radio" name="sexo" value="mujer"><br>
+            <input type="submit" name="enviar" value="Enviasr Datos"><br>
+                  
+        </form>
     </body>
 </html>
